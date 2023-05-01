f6d25e70d30f0b0edf146e2249436bd6d6d0f182
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-05-01 22:01:55 +0600

    add chef data and routing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const chef = require('./Data/chef.json')
 const app = express();
 const PORT = 5000;
 
@@ -8,8 +8,12 @@ app.use(cors());
 
 app.get("/", (req, res)=>{
     res.send("<h1>Legend Chef Running</h1>")
-
 })
+
+app.get("/chef", (req, res)=>{
+    res.send(chef)
+})
+
 app.listen(PORT, () => {
     console.log(`server is running in ${PORT} port`);
 })
