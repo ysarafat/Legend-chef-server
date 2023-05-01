cef12b7443ac2d1b8869754119c7a4800ba50ba0
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-05-01 20:16:23 +0600

    set home routing

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const PORT = 5000;
 
 app.use(cors());
 
+app.get("/", (req, res)=>{
+    res.send("<h1>Legend Chef Running</h1>")
 
+})
 app.listen(PORT, () => {
     console.log(`server is running in ${PORT} port`);
 })
